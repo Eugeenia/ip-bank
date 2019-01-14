b498446352af3ea73fd67d5d9a121084d3070a61
Eugeenia <jain2101@yandex.ru>
2019-01-14 19:49:12 +0500

    Angular is live

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { HeaderComponent } from './page/header/header.component';
+import { PaymentComponent } from './page/payment/payment.component';
+import { InfoAboutCompanyComponent } from './page/info-about-company/info-about-company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    HeaderComponent,
+    PaymentComponent,
+    InfoAboutCompanyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/page/info-about-company/info-about-company.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-about-company',
+  templateUrl: './info-about-company.component.html',
+  styleUrls: ['./info-about-company.component.css']
+})
+export class InfoAboutCompanyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
